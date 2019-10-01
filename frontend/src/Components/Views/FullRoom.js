37d7f4d6759f47f3c6d9906f4fa1db80969c4a43
Axis <atonymartin20@yahoo.com>
2019-10-01 15:45:56 -0500

    Completed Player Data and Room Data views

--- frontend/src/Components/Views/FullRoom.js
+++ frontend/src/Components/Views/FullRoom.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import { AppContext } from '../Context/AppContext.js';
 import { ViewPanelDiv, CurrentRoomTitleBar, CurrentRoomH2, CurrentRoomLi, CurrentRoomSpan } from '../StyledComponents';
-import InventoryList from '../CurrentRoom/InventoryList.js';
+import FullRoomInventoryList from './FullRoomInventoryList.js';
 
 class FullRoom extends React.Component{
     render(){
-        console.log(this.context.state.currentRoomData)
+        console.log(this.context.state.currentRoomData.exits)
         return (
             <ViewPanelDiv>
-
+                <CurrentRoomTitleBar>
+                    <CurrentRoomH2>{this.context.state.currentRoomData.title}</CurrentRoomH2>
+                    <CurrentRoomH2>{this.context.state.currentRoomData.coordinates}</CurrentRoomH2>
+                </CurrentRoomTitleBar>
+                <ul>
+                    <CurrentRoomLi><CurrentRoomSpan>Room ID:</CurrentRoomSpan>&nbsp;&nbsp;{this.context.state.currentRoomData.room_id}</CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Description:</CurrentRoomSpan>&nbsp;&nbsp;{this.context.state.currentRoomData.description}</CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Cooldown:</CurrentRoomSpan>&nbsp;&nbsp;{this.context.state.currentRoomData.cooldown}</CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Elevation:</CurrentRoomSpan>&nbsp;&nbsp;{this.context.state.currentRoomData.elevation}</CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Terrain:</CurrentRoomSpan>&nbsp;&nbsp;{this.context.state.currentRoomData.terrain}</CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Players:</CurrentRoomSpan> 
+                        <FullRoomInventoryList ListType='players' />
+                    </CurrentRoomLi>  
+                    <CurrentRoomLi><CurrentRoomSpan>Exits:</CurrentRoomSpan> 
+                        <FullRoomInventoryList ListType='exits' />
+                    </CurrentRoomLi>  
+                    <CurrentRoomLi><CurrentRoomSpan>Items:</CurrentRoomSpan> 
+                        <FullRoomInventoryList ListType='items' />
+                    </CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Messages:</CurrentRoomSpan> 
+                        <FullRoomInventoryList ListType='messages' />
+                    </CurrentRoomLi>
+                    <CurrentRoomLi><CurrentRoomSpan>Errors:</CurrentRoomSpan> 
+                        <FullRoomInventoryList ListType='errors' />
+                    </CurrentRoomLi>
+                </ul>
             </ViewPanelDiv>
         )
     }
 }
 
-// {room_id: 343, title: "A misty room", description: "You are standing on grass and surrounded by a dens…u can barely make out the exits in any direction.", coordinates: "(53,53)", elevation: 0, …}
-// cooldown: 1
-// coordinates: "(53,53)"
-// description: "You are standing on grass and surrounded by a dense mist. You can barely make out the exits in any direction."
-// elevation: 0
-// errors: []
-// exits: (2) ["s", "w"]
-// items: ["regular egg"]
-// messages: []
-// players: []
-// room_id: 343
-// terrain: "NORMAL"
-// title: "A misty room"
-// __proto__: Object
 FullRoom.contextType = AppContext;
 
 export default FullRoom;
---
+++ frontend/src/Components/Views/FullRoomInventoryList.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import { CurrentRoomItemPickUpButton } from '../StyledComponents';
+import { AppContext } from '../Context/AppContext.js';
+
+class FullRoomInventoryList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userData: {},
+            disablePickUpButton: false,
+        }
+    }
+
+    UpdateOnPickUp({item}) {
+        this.setState({
+            disablePickUpButton: true
+        })
+        this.context.PickUpItem({item});
+        setTimeout(() => {
+            this.context.GetInitalRoomData();
+        },this.context.state.cooldown + 5600)
+        setTimeout(() => {
+            this.context.GetUserData();
+        },this.context.state.cooldown + 500)
+        setTimeout(() => {
+            this.setState({
+                disablePickUpButton: false
+            })
+        }, 10000)
+    }
+    render() {
+        // Checks to see if this.context.state.currentRoomData has been filled in yet.
+        if(Object.keys(this.context.state.currentRoomData).length !== 0) {
+            // Checks ListType being passed through props
+            if (this.props.ListType === "players") {
+                if ((this.context.state.currentRoomData.players).length > 5) {
+                    let playerSpan = ''
+                    for(let i = 0; i < 5; i++) {
+                        playerSpan += `${this.context.state.currentRoomData.players[i]}, `
+                    }
+                    let remainingPlayers = this.context.state.currentRoomData.players.length - 5;
+                    return (
+                        <span>&nbsp;&nbsp;{playerSpan} and {remainingPlayers} more players.</span>
+                    )
+                }
+                else if ((this.context.state.currentRoomData.players).length > 0) {
+                    let playerSpan = ''
+                    this.context.state.currentRoomData.players.map(player => {
+                        return playerSpan += `${player}, ${'\xa0'} `
+                    })
+                
+                    const removeTrailingComma = (string) => {
+                        let n = string.lastIndexOf(",");
+                        let a = string.substring(0, n);
+                        return a;
+                    }
+                    return (
+                        <span>&nbsp;&nbsp;{removeTrailingComma(playerSpan)}</span>
+                    );
+                }
+                else {
+                    return(
+                        <span>&nbsp;&nbsp;No players</span>
+                    );
+                }
+            }
+
+            if (this.props.ListType === "exits") {
+                if (Object.keys(this.context.state.currentRoomData.exits).length > 0) {
+                    return (
+                        <ul>
+                            {this.context.state.currentRoomData.exits.map(exit => {
+                                return <li key={Math.random()}>{exit}</li>
+                            })}
+                        </ul>
+                    );
+                }
+                else {
+                    return(
+                        <span>&nbsp;&nbsp;No items</span>
+                    );
+                }
+            }
+
+            // Checks ListType being passed through props
+            if (this.props.ListType === "items") {
+                if (Object.keys(this.context.state.currentRoomData.items).length > 0) {
+                    return (
+                        <ul>
+                            {this.context.state.currentRoomData.items.map(item => {
+                                return <li key={Math.random()}>{item}<CurrentRoomItemPickUpButton onClick={() => {this.UpdateOnPickUp({item})}} disabled={this.state.disablePickUpButton === true}>Pick Up</CurrentRoomItemPickUpButton></li>
+                            })}
+                        </ul>
+                    );
+                }
+                else {
+                    return(
+                        <span>&nbsp;&nbsp;No items</span>
+                    );
+                }
+            }
+
+            // Checks ListType being passed through props
+            if (this.props.ListType === "messages") {
+                if ((this.context.state.currentRoomData.messages).length > 1) {
+                    return (
+                        <ul>
+                            {this.context.state.currentRoomData.messages.map(message => {
+                                return <li key={Math.random()}>{message}</li>
+                            })}
+                        </ul>
+                    );
+                }
+                else if ((this.context.state.currentRoomData.messages).length === 1) {
+                    let messageSpan = ''
+                    this.context.state.currentRoomData.messages.map(message => {
+                        return messageSpan += `${message}`
+                    })
+                        
+                    return (
+                        <span>&nbsp;&nbsp;{messageSpan}</span>
+                        );
+                }
+                else {
+                    return(
+                        <span>&nbsp;&nbsp;No messages</span>
+                    );
+                }
+            }
+
+            // Checks ListType being passed through props
+            if (this.props.ListType === "errors") {
+                if ((this.context.state.currentRoomData.errors).length > 1) {
+                    return (
+                        <ul>
+                            {this.context.state.currentRoomData.errors.map(error => {
+                                return <li key={Math.random()}>{error}</li>
+                            })}
+                        </ul>
+                    );
+                }
+                else if ((this.context.state.currentRoomData.errors).length === 1) {
+                    let errorSpan = ''
+                    this.context.state.currentRoomData.errors.map(error => {
+                        return errorSpan += `${error}`
+                    })
+                        
+                    return (
+                        <span>&nbsp;&nbsp;{errorSpan}</span>
+                        );
+                }
+                else {
+                    return(
+                        <span>&nbsp;&nbsp;No errors</span>
+                    );
+                }
+            }
+
+            // If the prop ListType doesn't match "players", "items", "exits, ""messages", or "errors" return null.
+            else {
+                return null
+            }
+        }
+
+        // if this.context.state.currentRoomData hasn't been filled in yet return null
+        else {
+            return null
+        }
+    }
+}
+
+FullRoomInventoryList.contextType = AppContext;
+
+export default FullRoomInventoryList;
